862e92afa3912c4791ae6810068494e0b845b3e3
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2024-01-04 15:34:39 +0900

    minor refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-{
+document.addEventListener("DOMContentLoaded", () => {
   const makeDizzyTrack = (callback: (sign: -1 | 0 | 1) => void) => {
     const SCORE_CNT = 4;
     const SCORE_THRESH = 11;
@@ -36,49 +36,46 @@
       prevY = y;
     };
   };
-  document.addEventListener("DOMContentLoaded", () => {
-    const $faceArea = document.getElementById("oo_face_area")!;
-    const $eye1 = document.getElementById(
-      "eye_black_1",
-    )! as Element as SVGUseElement;
-    const $eye2 = document.getElementById(
-      "eye_black_2",
-    )! as Element as SVGUseElement;
-    const getOOArea = () => $faceArea.getBoundingClientRect();
-    const moveEyes = (r: number, theta: number) => {
-      const dx = r * Math.cos(theta);
-      const dy = r * Math.sin(theta) * 1.1;
-      $eye1.setAttribute("x", `${dx}`);
-      $eye1.setAttribute("y", `${dy}`);
-      $eye2.setAttribute("x", `${dx}`);
-      $eye2.setAttribute("y", `${dy}`);
-    };
-    const $eye0 = document.getElementById(
-      "eye_black",
-    )! as Element as SVGElement;
-    const dizzyTrack = makeDizzyTrack((sign) => {
-      const className =
-        sign === 0 ? "" : sign > 0 ? "rot-clock" : "rot-anticlock";
-      if ($eye0.getAttribute("class") !== className) {
-        $eye0.setAttribute("class", className);
-      }
-    });
-    const eyeTrack = (mx: number, my: number) => {
-      const { left, top, width, height } = getOOArea();
-      const dx = mx - (left + width / 2);
-      const dy = my - (top + height / 2);
-      const r = Math.sqrt(dx * dx + dy * dy) / width;
-      moveEyes(25 * Math.atan(r * 2), Math.atan2(dy, dx));
-      dizzyTrack(dx / width, dy / height);
-    };
-    window.addEventListener("mousemove", (ev) => {
-      eyeTrack(ev.clientX, ev.clientY);
-    });
-    window.addEventListener("touchstart", (ev) => {
-      eyeTrack(ev.touches[0].clientX, ev.touches[0].clientY);
-    });
-    window.addEventListener("touchmove", (ev) => {
-      eyeTrack(ev.touches[0].clientX, ev.touches[0].clientY);
-    });
+
+  const $faceArea = document.getElementById("oo_face_area")!;
+  const getOOArea = () => $faceArea.getBoundingClientRect();
+
+  const $oo = document.getElementById("oo")! as Element as SVGSVGElement;
+  const $eye1 = $oo.getElementById("eye_black_1")! as SVGUseElement;
+  const $eye2 = $oo.getElementById("eye_black_2")! as SVGUseElement;
+  const moveEyes = (r: number, theta: number) => {
+    const dx = r * Math.cos(theta);
+    const dy = r * Math.sin(theta) * 1.1;
+    $eye1.setAttribute("x", `${dx}`);
+    $eye1.setAttribute("y", `${dy}`);
+    $eye2.setAttribute("x", `${dx}`);
+    $eye2.setAttribute("y", `${dy}`);
+  };
+
+  const $eye0 = $oo.getElementById("eye_black")! as SVGElement;
+  const dizzyTrack = makeDizzyTrack((sign) => {
+    const className =
+      sign === 0 ? "" : sign > 0 ? "rot-clock" : "rot-anticlock";
+    if ($eye0.getAttribute("class") !== className) {
+      $eye0.setAttribute("class", className);
+    }
   });
-}
+
+  const eyeTrack = (mx: number, my: number) => {
+    const { left, top, width, height } = getOOArea();
+    const dx = mx - (left + width / 2);
+    const dy = my - (top + height / 2);
+    const r = Math.sqrt(dx * dx + dy * dy) / width;
+    moveEyes(25 * Math.atan(r * 2), Math.atan2(dy, dx));
+    dizzyTrack(dx / width, dy / height);
+  };
+  window.addEventListener("mousemove", (ev) => {
+    eyeTrack(ev.clientX, ev.clientY);
+  });
+  window.addEventListener("touchstart", (ev) => {
+    eyeTrack(ev.touches[0].clientX, ev.touches[0].clientY);
+  });
+  window.addEventListener("touchmove", (ev) => {
+    eyeTrack(ev.touches[0].clientX, ev.touches[0].clientY);
+  });
+});
