6e5924469657053e90b89267aa73f850810085b8
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2024-07-21 00:58:23 +0900

    Migrate to ESLint flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+/* eslint-disable */
+
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  eslintConfigPrettier,
+);
